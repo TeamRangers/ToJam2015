56d4696621c9f86c6f588d8852befe709cb53ed8
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-01 16:27:11 -0400

    Fixed the projectiles to have correct z axis scale. Implemented spawning projectiles in the correct area of the screen. Still need to fix the spawning position.

--- Tojam2015/Assets/Scripts/playerController.cs
+++ Tojam2015/Assets/Scripts/playerController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class playerController : MonoBehaviour {
+	public GameObject projectileObject;
 
 	// Use this for initialization
 	void Start () {
@@ -10,8 +11,18 @@ public class playerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1")){
 
+
+			
+
+		if (Input.GetButtonDown("Fire1")){
+			RaycastHit2D hit = Physics2D.Raycast(transform.position,  Camera.main.ScreenToWorldPoint (Input.mousePosition));
+			Vector3 target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			if (hit.point != null){
+				target.z = 0;
+				Quaternion projectileRotation = Quaternion.LookRotation(target - transform.position);
+				Instantiate(projectileObject, transform.position * 0, projectileRotation);
+			}
 		}
 	}
 }
