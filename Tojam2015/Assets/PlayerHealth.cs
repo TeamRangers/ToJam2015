238eac9e1e6c0964c1cb657327330cf8536f87c0
alexcfyung <alexcfyung@hotmail.com>
2015-05-01 18:51:35 -0400

    Finish player health

---
+++ Tojam2015/Assets/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startHealth = 100;
+	public int currentHealth;
+
+	bool isDead;
+
+	void Awake () {
+		currentHealth = startHealth;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TakeDamage (int amount)
+	{
+		currentHealth -= amount;
+
+		if (currentHealth <= 0 && !isDead) {
+			Death();
+		}
+	}
+
+	void Death() {
+		isDead = true;
+
+		gameObject.SetActive(false);
+	}
+}
