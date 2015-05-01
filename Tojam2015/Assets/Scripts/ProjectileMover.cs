d358da2055f77281f13ded6d7367fb8d4955dfea
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-01 15:37:21 -0400

    Added a simple sphere to temporarily serve as a projectile.
    The sphere moves to the right of its transform by getting some initial acceleration.

---
+++ Tojam2015/Assets/Scripts/ProjectileMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMover : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().AddForce(transform.right * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+
+
+	}
+}
