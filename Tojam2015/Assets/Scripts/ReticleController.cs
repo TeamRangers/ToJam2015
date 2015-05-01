2aeea3c102559fe20961d4bdeeff8fdf16db1bf2
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-01 18:08:45 -0400

    Created a reticle for player 1 which is moved using the mouse.

---
+++ Tojam2015/Assets/Scripts/ReticleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReticleController : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float HorizontalMovement = Input.GetAxis("Mouse X");
+		float VerticalMovement = Input.GetAxis("Mouse Y");
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(HorizontalMovement, VerticalMovement) * speed;
+	}
+}
--- Tojam2015/Assets/Scripts/playerController.cs
+++ Tojam2015/Assets/Scripts/playerController.cs
@@ -7,7 +7,9 @@ public class playerController : MonoBehaviour {
 	// Use this for initialization
 	public float speed;
 	public float jumpSpeed;
-	
+
+	public GameObject reticle;
+
 	private Rigidbody2D rb;
 	
 	void Start ()
@@ -35,7 +37,8 @@ public class playerController : MonoBehaviour {
 		}
 
 		if (Input.GetButtonDown("Fire1")){
-			Vector3 target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			//Vector3 target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Vector3 target = reticle.transform.position;
 			target.z = 0;
 			Quaternion projectileRotation = Quaternion.LookRotation(target - transform.position);
 			Instantiate(projectileObject, Vector3.MoveTowards(transform.position, target, 1), projectileRotation);
