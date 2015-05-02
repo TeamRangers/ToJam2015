de29eb3c6b351493cdbbe8e0752d3b9f6d71a1ca
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-01 21:29:42 -0400

    Changed font and colors for the main menu buttons.

--- Tojam2015/Assets/Scripts/MainMenuScripts.cs
+++ Tojam2015/Assets/Scripts/MainMenuScripts.cs
@@ -1,15 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class MainMenuScripts : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void StartGame()
+	{
+		Application.LoadLevel("Scene1");
 	}
 }
