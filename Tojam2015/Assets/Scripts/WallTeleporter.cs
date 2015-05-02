90e366c6b3690678812453b444fa95f7c40680a6
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-02 14:36:39 -0400

    Added a wall teleported script and changed walls to triggers.

---
+++ Tojam2015/Assets/Scripts/WallTeleporter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallTeleporter : MonoBehaviour {
+
+	public GameObject oppositeWall;
+	public bool teleportX; //Determine whether we teleport horizontally or vertically.
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
