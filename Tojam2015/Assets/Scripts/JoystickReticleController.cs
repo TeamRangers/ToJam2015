abc27eb6bd00c7836143f51fbd282cb0188a87db
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-02 17:54:37 -0400

    Added a JoysickReticle controller script for better reticle control when using Joystics.

---
+++ Tojam2015/Assets/Scripts/JoystickReticleController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickReticleController : MonoBehaviour {
+
+	public string Horizontal, Vertical;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = new Vector3(Input.GetAxis(Horizontal), Input.GetAxis(Vertical), 0);
+		transform.position = player.transform.position + direction * 2;
+	}
+}
