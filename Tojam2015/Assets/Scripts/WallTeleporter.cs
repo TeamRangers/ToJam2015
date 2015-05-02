3ce8785c0c6e6c7eec6335b57de372e2aacbedf9
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-02 14:48:45 -0400

    Added basic code for wall teleportation script.

--- Tojam2015/Assets/Scripts/WallTeleporter.cs
+++ Tojam2015/Assets/Scripts/WallTeleporter.cs
@@ -5,6 +5,7 @@ public class WallTeleporter : MonoBehaviour {
 
 	public GameObject oppositeWall;
 	public bool teleportX; //Determine whether we teleport horizontally or vertically.
+	public static float offset = 50;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +15,28 @@ public class WallTeleporter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player"){
+		}
+		Vector2 otherPosition = other.transform.position;
+		if (teleportX){
+			if (gameObject.transform.position.x < oppositeWall.gameObject.transform.position.x){ //This wall is on the left
+				otherPosition.x = oppositeWall.transform.position.x - offset;
+			}
+			else {otherPosition.x = oppositeWall.transform.position.x + offset;}
+		}
+		else
+		{
+			if (gameObject.transform.position.y < oppositeWall.gameObject.transform.position.y){ //This wall is below the other wall
+				otherPosition.y = oppositeWall.transform.position.y - offset;
+			}
+			else{
+				otherPosition.y = oppositeWall.transform.position.y + offset;
+			}
+		}
+
+		other.transform.position = otherPosition;
 	}
 }
