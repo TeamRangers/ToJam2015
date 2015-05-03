718aeb0c6f0233cf776e3407556a9532773861b7
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-03 17:01:41 -0400

    Added a main menu manager which properly sets the onClick delegates for buttons.

---
+++ Tojam2015/Assets/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.FindGameObjectWithTag("OnePlayerButton").GetComponent<Button>().onClick.AddListener(ArenaManager.Instance.LoadOnePlayer);
+		GameObject.FindGameObjectWithTag("TwoPlayerButton").GetComponent<Button>().onClick.AddListener(ArenaManager.Instance.LoadTwoPlayer);
+		//GameObject.FindGameObjectWithTag("MuteButton").GetComponent<Button>().onClick.AddListener(ArenaManager.Instance.);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
