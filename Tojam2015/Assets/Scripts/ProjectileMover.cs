727752f73857d8eb78d7657ebc8c80fae546d6e3
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-05-03 14:37:14 -0400

    Added the sound prefabs and code that will supposedly play them.

--- Tojam2015/Assets/Scripts/ProjectileMover.cs
+++ Tojam2015/Assets/Scripts/ProjectileMover.cs
@@ -5,6 +5,11 @@ public class ProjectileMover : MonoBehaviour {
 	public float speed;
 	public int damage = 20;
 	public int lifeTime = 2;
+
+	public AudioClip[] destructionSounds;
+	public AudioClip destSnd;
+
+	AudioSource _audioSrc;
     
     ForceField _forceField;
     ConstantForce2D _cf2D;
@@ -21,10 +26,11 @@ public class ProjectileMover : MonoBehaviour {
 	void Start () {
         _rb2D.velocity = transform.forward * speed;
         _forceField = GameObject.FindGameObjectWithTag("ForceField").GetComponent<ForceField>();
+		_audioSrc = GetComponent<AudioSource>(); 
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
-
+		_audioSrc.PlayOneShot(destSnd);
 		if (coll.gameObject.tag.Contains("Player")) {
 
 			PlayerHealth playerHealth = coll.gameObject.GetComponent<PlayerHealth>();
@@ -38,6 +44,7 @@ public class ProjectileMover : MonoBehaviour {
 		Destroy(gameObject);
 	}
 
+
 	// Update is called once per frame
 	void Update () {
 	
@@ -46,5 +53,15 @@ public class ProjectileMover : MonoBehaviour {
 	void FixedUpdate()
     {
         _cf2D.force = _forceField.GetForce(transform.position);
+	}
+
+	void PlayDestructionSound(){
+		Debug.Log("Playing sound");
+		if (destructionSounds.Length > 0){
+			destructionSounds[0].LoadAudioData();
+			_audioSrc.clip = destructionSounds[0];
+			_audioSrc.PlayOneShot(destSnd);
+			
+		}
 	}
 }
